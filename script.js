d68d33e8b9c5274cf39807f948f30522baf5cc42
Gillian Laming <gdlaming@wustl.edu>
2019-06-30 14:14:54 -0500

    yEEEEt the js is working

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 500) {
+        if($(window).scrollTop() > 600) {
             $("#header").addClass("active");
+            $("#header").removeClass("regular");
+            console.log("yeet");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
-           $("#nheader").removeClass("active");
+            $("#header").addClass("regular");
+           $("#header").removeClass("active");
+           
+           console.log("yeeted")
         }
     });
 });
+
+// $(window).on("scroll", function() {
+//     if($(window).scrollTop() > 50) {
+//         $("#header").addClass("active");
+//     } else {
+//         //remove the background property so it comes transparent again (defined in your css)
+//        $("#header").removeClass("active");
+//     }
+// });
